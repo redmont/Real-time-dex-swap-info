bf4aa5fddabe44681492cbac1b70625cc9ab8eef
redmont <deepanshusingh@192.168.1.76>
2022-12-15 04:09:41 +0530

    real time

--- index.js
+++ index.js
@@ -1,56 +1,70 @@
 const {ethers} = require("ethers");
-const {
-    Route,
-    Trade,
-    FeeAmount,
-    nearestUsableTick,
-    Pool,
-    TickMath,
-    TickListDataProvider,
-    Tick,
-    TICK_SPACINGS,
-} = require("@uniswap/v3-sdk");
 const {TradeType, CurrencyAmount, Token, Percent} = require("@uniswap/sdk-core");
 const {AlphaRouter} = require("@uniswap/smart-order-router");
 
 require("dotenv").config();
 const JSBI = require("jsbi");
+const {getTokenInfo} = require("erc20-token-list");
 
 const provider = new ethers.providers.JsonRpcProvider(process.env.INFURA_URL);
 const router = new AlphaRouter({chainId: 1, provider: provider});
 
 const amountIn = ethers.utils.parseEther(process.env.AMOUNT_IN);
 
-//const fees = [FeeAmount.LOW, FeeAmount.MEDIUM, FeeAmount.HIGH];
-const fees = [FeeAmount.MEDIUM];
+const [token0info, token1info] = [
+    getTokenInfo(process.env.TOKEN0),
+    getTokenInfo(process.env.TOKEN1),
+];
 
 const token0 = new Token(
     Number(process.env.CHAIN_ID),
-    "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2",
-    18,
-    "WETH",
-    "Wrapped Ether"
+    token0info.address,
+    token0info.decimals,
+    token0info.symbol,
+    token0info.name
 );
 
 const token1 = new Token(
     Number(process.env.CHAIN_ID),
-    "0x6b175474e89094c44da98b954eedeac495271d0f",
-    18,
-    "DAI",
-    "Dai Stable Coin"
+    token1info.address,
+    token1info.decimals,
+    token1info.symbol,
+    token1info.name
 );
 
-const init = async (fee) => {
-    const token0Amount = CurrencyAmount.fromRawAmount(token0, JSBI.BigInt(amountIn));
-    const route1 = await router.route(token0Amount, token1, TradeType.EXACT_INPUT);
+const timeout = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+let count = 0;
+const init = async (token0, token1) => {
+    console.log(
+        `[${count}] Fetching updated details for ${token0info.symbol}/${token1info.symbol} ...`
+    );
 
-    console.log(`Quote Exact In: ${route1.quote.toFixed(2)}`);
-    console.log(`Gas Adjusted Quote In: ${route1.quoteGasAdjusted.toFixed(2)}`);
-    console.log(`Gas Used USD: ${route1.estimatedGasUsedUSD.toFixed(6)}`);
-    console.log(`Price Impact: ${route1.trade.priceImpact.toFixed(2)}`);
-    console.log(JSON.stringify(route1, null, 2));
+    const token0Amount = CurrencyAmount.fromRawAmount(token0, JSBI.BigInt(amountIn));
+    const route = await router.route(token0Amount, token1, TradeType.EXACT_INPUT, {
+        type: 0,
+        slippageTolerance: new Percent(parseInt(process.env.SLIPPAGE_TOLERANCE), 100),
+        deadline: Math.floor(Date.now() / 1000 + parseInt(process.env.TRANSACTION_DEADLINE)),
+    });
+
+    console.log(`=========================================`);
+    console.log(`Quote Exact: ${route.quote.toFixed(2)} ${token1info.symbol}`);
+    console.log(`Gas Adjusted Quote: ${route.quoteGasAdjusted.toFixed(2)} ${token1info.symbol}`);
+    console.log(`Gas Used USD: ${route.estimatedGasUsedUSD.toFixed(6)}`);
+    console.log(`Price Impact: ${route.trade.priceImpact.toFixed(2)}`);
+    console.log(`\n`);
+    console.log(`Gas Used: ${route.estimatedGasUsed}`);
+    console.log(`Gas Price: ${ethers.utils.formatUnits(route.gasPriceWei, "gwei")} gwei`);
+    console.log(`=========================================\n`);
+    await timeout(1000);
+    count++;
+    await init(token0, token1);
 };
 
-fees.forEach((fee) => {
-    init(fee);
-});
+console.log(`\n=========================================`);
+console.log(`Pair: ${token0info.symbol}/${token1info.symbol}`);
+console.log(`Input: ${process.env.AMOUNT_IN} ${token0info.symbol}`);
+console.log(`SLIPPAGE TOLERANCE: ${process.env.SLIPPAGE_TOLERANCE}%`);
+console.log(`TRANSACTION DEADLINE: ${process.env.TRANSACTION_DEADLINE} sec`);
+console.log(`=========================================\n\n`);
+
+init(token0, token1);
